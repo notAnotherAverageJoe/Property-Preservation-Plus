1dbab169ec2f0f4029977da7425a22952f78d68f
joseph skokan <joeskokan20@gmail.com>
2024-09-06 14:14:21 -0400

    user auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,23 +3,27 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const authenticate = require("../middleware/authenticate");
 const companyController = require("../controllers/companyController");
-
 // Route to get all users
-router.get("/users", userController.getAllUsers);
+router.get("/users", authenticate, userController.getAllUsers);
 
 // Route to get a specific user by ID
-router.get("/users/:id", userController.getUserById);
+router.get("/users/:id", authenticate, userController.getUserById);
 
 // Route to create a new user
-router.post("/users", userController.createUser);
+router.post("/users", authenticate, userController.createUser);
 
 // Route to update a user
-router.put("/users/:id", userController.updateUser);
+router.put("/users/:id", authenticate, userController.updateUser);
 
 // Route to delete a user
-router.delete("/users/:id", userController.deleteUser);
+router.delete("/users/:id", authenticate, userController.deleteUser);
 
 // Route to update company and refresh token
-router.post("/updateCompany", authenticate, companyController.updateCompany);
+router.post(
+  "/updateCompany",
+  authenticate,
+  authenticate,
+  companyController.updateCompany
+);
 
 module.exports = router;
