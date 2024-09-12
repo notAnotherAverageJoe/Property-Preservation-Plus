4b46eb12463e9e515e857ba429e7c974cf2a00ae
joseph skokan <joeskokan20@gmail.com>
2024-09-12 17:19:30 -0400

    css styles

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,15 @@
-// src/pages/Main.js
 import React from "react";
 import { Link } from "react-router-dom";
+import "../pagesCSS/Home.css"; // Ensure this path is correct
 
 const Main = () => {
   return (
-    <div>
-      <h1>Welcome to the main page!</h1>
+    <div className="containerHome">
+      <div className="triangleBackground"></div>
+      <h1>Welcome to </h1>
+      <h1>Property Preservation Plus!</h1>
+      <h2>The Only Property Management System You'll Ever Need!</h2>
+      <h3>info to be added</h3>
       <h2>
         <Link to="/login">Login</Link>
       </h2>
