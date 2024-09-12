2979a9bc49cdc65ed09e4cc313d3a709f6ca80b8
joseph skokan <joeskokan20@gmail.com>
2024-09-11 23:38:39 -0400

    Added searchbar test

---
+++ client/src/__tests__/utils/SearchBar.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import SearchBar from "../../components/SearchBar";
+
+test("renders SearchBar with correct placeholder and handles input change", () => {
+  const mockOnSearchChange = jest.fn();
+  const placeholderText = "Search...";
+  const searchTerm = "";
+
+  render(
+    <SearchBar
+      searchTerm={searchTerm}
+      onSearchChange={mockOnSearchChange}
+      placeholderText={placeholderText}
+    />
+  );
+
+  // Check if the input element is rendered with the correct placeholder
+  const inputElement = screen.getByPlaceholderText(placeholderText);
+  expect(inputElement).toBeInTheDocument();
+
+  fireEvent.change(inputElement, { target: { value: "test" } });
+
+  // Check if the onSearchChange function is called with the correct value
+  expect(mockOnSearchChange).toHaveBeenCalledWith("test");
+});
